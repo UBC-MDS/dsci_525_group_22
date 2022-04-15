b43490963e96ea983557d932c603e2fb23c8b3a8
mahsasarafrazi <Ma.sarafrazi@gmail.com>
2022-04-15 08:35:20 -0700

    minor change on reading file

--- notebooks/mahsa/Milestone3-Task4-filled.py
+++ notebooks/mahsa/Milestone3-Task4-filled.py
@@ -12,7 +12,7 @@ import numpy as np
 ## to read from the bucket, if so provide with your credentials and pass as storage_options=aws_credentials
 # aws_credentials = {"key": "","secret": "","token":""}
 ## here 100 data points for testing the code
-aws_credentials = {"key": "ASIA2AE6FPIPHAMBDRWB","secret": "V9Y81NOxarLYd6PJYw8v4xjwcBuxqfK8r4KsTG1A","token":"FwoGZXIvYXdzEOr//////////wEaDD6cH8kLcDm6lGT4ryLHAZrrux7nK0T/WP9r9/hShQkJuvViK36Jd8f7oWwlRTmFT+LhBH/Uq99dvceAYlXM4Lo2Z8HP4I6zAuJNyo2VWPTkRFyn4MijnDDU+8tppSdxnDczbdYE1qJ7XWPUBxj4k3XOTQA5XnnXD9w03BsT1hUdU54j21VF2TiYf7pZDCAayi0kb7KgYVvKtlIy5LrBSV4aOuPicNd775ganm/NDb+AQlF4X0AY+rP6HdTNTvjGDdebO8x4jeoeoYM3p5Ar1o1u79fuj7oove/ikgYyLSfcJydpDBX+Vq5CNQdvKOXo2P4uifdRHKBEFEFXftrx0yNbIahc+nCM5Tz4Jg=="}
+# aws_credentials = {"key": "","secret": "","token":""}
 pandas_df = pd.read_csv("s3://mds-s3-22/output/ml_data_SYD.csv", storage_options = aws_credentials, index_col=0, parse_dates=True).dropna()
 # pandas_df = pd.read_csv("s3://xxxx/output/ml_data_SYD.csv", index_col=0, parse_dates=True).dropna()
 feature_cols = list(pandas_df.drop(columns="observed").columns)
